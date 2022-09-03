ebd299ba2200520f8f76595bb6638f461fda0252
Vivi2022-byte <vivianne.sistemas@gmail.com>
2022-09-03 09:05:53 -0300

    Aula 09

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,14 +1,17 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Moment } from 'src/app/Moment';
 
 @Component({
   selector: 'app-moment-form',
   templateUrl: './moment-form.component.html',
-  styleUrls: ['./moment-form.component.css']
+  styleUrls: ['./moment-form.component.css'],
 })
 export class MomentFormComponent implements OnInit {
+  @Output() onSubmit = new EventEmitter<Moment>()
   @Input() btnText!: string;
+  
   momentForm!: FormGroup;
 
   constructor() { }
@@ -31,11 +34,19 @@ export class MomentFormComponent implements OnInit {
     return this.momentForm.get('description')!;
   }
 
+  onFileSelected(event:any){
+      
+    const file: File = event.target.files[0]
+    this.momentForm.patchValue({ image: file})
+  }
+
   submit() {
     if(this.momentForm.invalid){
       return;
     }
-    console.log("Enviou formulário");
+    console.log(this.momentForm.value);
+
+    this.onSubmit.emit(this.momentForm.value);
   }
 
 }

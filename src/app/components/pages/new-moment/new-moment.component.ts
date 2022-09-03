8099daf6452423b26ecd2fe3cf7e00ce3fdfdc17
Vivi2022-byte <vivianne.sistemas@gmail.com>
2022-09-03 10:03:54 -0300

    Aula 11

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Moment } from 'src/app/Moment';
 
 import { MomentService } from 'src/app/services/moment.service';
+
+import { MessagesService } from 'src/app/services/messages.service';
 
 @Component({
   selector: 'app-new-moment',
@@ -11,7 +14,7 @@ import { MomentService } from 'src/app/services/moment.service';
 export class NewMomentComponent implements OnInit {
   btnText = 'Compartilhar!';
 
-  constructor(private momentService: MomentService) { }
+  constructor(private momentService: MomentService, private messagesService: MessagesService, private router: Router ) { }
 
   ngOnInit(): void {}
 
@@ -25,13 +28,12 @@ export class NewMomentComponent implements OnInit {
       formData.append('image', moment.image);
     }
 
-    // todo
 
     await this.momentService.createMomment(formData).subscribe();
 
-    // exibir msg
+    this.messagesService.add('Momento adicionado com sucesso!');
 
-    // redirect
+  this.router.navigate(['/']);
     
   }
 
